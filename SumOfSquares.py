f2b110912158b3d532cbd584674b8b285dd7aac1
Oleksandr Chakalov <sasha.chakalov@gmail.com>
2021-02-16 18:48:27 +0200

    Basic things

---
+++ SumOfSquares.py
@@ -0,0 +1,30 @@
+# Return the sum of
+# square of first n
+# natural numbers
+def squaresum(n):
+    # Iterate i from 1
+    # and n finding
+    # square of i and
+    # add to sum.
+    sm = 0
+    for i in range(1, n + 1):
+        sm = sm + (i * i)
+
+    return sm
+
+
+# Driven Program
+n = int(input("Enter number pls: "))
+print(squaresum(n))
+
+# 2nd variant
+# # Return the sum of
+# # square of first n
+# # natural numbers
+# def squaresum(n):
+#     return (n * (n + 1) * (2 * n + 1)) // 6
+#
+#
+# # Driven Program
+# n = int(input("Enter number pls: "))
+# print(squaresum(n))
