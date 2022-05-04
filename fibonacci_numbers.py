6a612dd3368acebd00dcb48157d6bfd6107e9d94
Oleksandr the Great <32864151+OleksandrChakalov@users.noreply.github.com>
2022-05-04 16:32:44 +0300

    Update fibonacci_numbers.py

--- fibonacci_numbers.py
+++ fibonacci_numbers.py
@@ -2,9 +2,8 @@ fib0 = fib1 = 1
 n = int(input("Please enter number: "))
 if n == 0:
     print("Please try one more time")
-
 else:
     print(fib0, fib1, end=" ")
-    for i in range(n):
+    for i in range(n-2):
         fib0, fib1 = fib1, fib1+fib0
         print(fib1, end=" ")
